05167c7bdd42ed368eb02dca3b1895c43c1fd50e
Silas Kraume <silas.kraume1552@gmail.com>
2022-11-29 12:41:58 +0100

    added console entry-point

--- prolog_kernel/__main__.py
+++ prolog_kernel/__main__.py
@@ -1,4 +1,9 @@
 from ipykernel.kernelapp import IPKernelApp
 from prolog_kernel.kernel import PrologKernel
 
+
+def entry_point():
+    IPKernelApp.launch_instance(kernel_class=PrologKernel)
+
+
 IPKernelApp.launch_instance(kernel_class=PrologKernel)
